81d48ec66884283e155cefe3399b1210a771f814
Daniel Yoo <jarkassp2@gmail.com>
2025-01-23 12:37:16 -0500

    chore: cleanup comments

--- src/stories/DataEntry.stories.tsx
+++ src/stories/DataEntry.stories.tsx
@@ -17,14 +17,10 @@ const meta = {
   title: "Data Entry",
   component: DataEntry,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {},
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: {
     location: {
       latitude: 0,
