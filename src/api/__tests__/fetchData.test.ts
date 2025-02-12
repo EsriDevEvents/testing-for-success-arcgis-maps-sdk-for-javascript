9e6b896ea9e92ac8db9a26ee2e48a092a51372fd
Michael Dandini <mdandini@esri.com>
2025-02-12 14:55:33 -0500

    Adding Fetch Data tests

---
+++ src/api/__tests__/fetchData.test.ts
@@ -0,0 +1,71 @@
+import fetchMock from "@fetch-mock/vitest";
+import { afterAll, assert, beforeAll, beforeEach, describe, test } from "vitest";
+import { loadObservations, saveObservation } from "../fetchData";
+
+
+describe("loadObservations", () => {
+  beforeAll(() => {
+    fetchMock.mockGlobal();
+  });
+
+  beforeEach(() => {
+    fetchMock.mockClear();
+    fetchMock.config.allowRelativeUrls = true;
+  });
+
+  afterAll(() => {
+    fetchMock.unmockGlobal();
+  });
+
+  test("it returns an array", async () => {
+    // Have fetchMock return a specific value
+    fetchMock.get("*", JSON.stringify([]));
+
+    const obs = await loadObservations()
+    assert.sameMembers(obs, []);
+
+    const callLog = fetchMock.callHistory.callLogs[0];
+    assert.exists(callLog);
+    assert.equal(callLog.url, "/load");
+  });
+
+  test("it throws an exception", async () => {
+    // Force fetch to throw an error
+    fetchMock.get("*", 500, "expected");
+
+    try {
+      await loadObservations()
+      assert.fail("No Exception thrown");
+    } catch (error) {
+    }
+  });
+});
+
+describe("saveObservation", () => {
+  const observation = { "name": "test" };
+
+  beforeAll(() => {
+    fetchMock.mockGlobal();
+  });
+  
+  beforeEach(() => {
+    fetchMock.mockClear();
+    fetchMock.config.allowRelativeUrls = true;
+  });
+
+  afterAll(() => {
+    fetchMock.unmockGlobal();
+  });
+
+  test("it sends the observations", async () => {
+    fetchMock.post("*", { response: "ok"});
+
+    const response = await saveObservation(observation);
+
+    assert.deepEqual(response, {response: "ok"});
+    const callLog = fetchMock.callHistory.callLogs[0];
+    assert.exists(callLog);
+    assert.equal(callLog.url, "/save");
+    assert.equal(callLog.options.body, JSON.stringify(observation));
+  });
+});
