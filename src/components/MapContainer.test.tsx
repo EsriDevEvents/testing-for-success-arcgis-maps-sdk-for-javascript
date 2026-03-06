ad299fadf058581f85fba628f64fac668b161544
Thorben Westerhuys <twesterhuys@esri.com>
2026-03-06 08:07:18 -0800

    wip

--- src/components/MapContainer.test.tsx
+++ src/components/MapContainer.test.tsx
@@ -83,14 +83,14 @@ describe("MapContainer", () => {
 
   it("opens popup when observation is clicked", async () => {
     const component = await render(
-      <div style={{ width: "400px", height: "720px" }}>
+      <div style={{ width: "800px", height: "600px" }}>
         <MapContainer
           location={{ latitude: 0, longitude: 0 }}
           setLocation={() => {}}
           observations={[
             {
-              latitude: 34.02771190164404,
-              longitude: -118.81032193749677,
+              latitude: 34.027,
+              longitude: -118.805,
               observation: "☀️",
             },
           ]}
@@ -101,7 +101,7 @@ describe("MapContainer", () => {
     const map = component.container.querySelector("arcgis-map")!;
     await expect.poll(() => map.updating, { timeout: 10_000 }).toBeFalsy();
 
-    await userEvent.click(map, { position: { x: 10, y: 10 } });
+    await userEvent.click(map, { position: { x: 400, y: 300 } });
 
     await expect.element(component.getByText("☀️")).toBeVisible();
     await expect.element(component.locator).toMatchScreenshot();
